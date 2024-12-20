1c7049c5f4019df0bcedb57d931829a2b9a59753
Benjamin <benjamin.gaymay@epitech.eu>
2024-12-20 15:43:48 +0100

    init

---
+++ src/components/PasswordField.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="password-field-container">
+    <label :for="id">{{ labelText }}</label>
+    <div>
+      <input
+        type="password"
+        :id="id"
+        :value="modelValue"
+        @input="updateModelValue"
+      />
+      <!-- TODO add a toogle visibility button here -->
+    </div>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  modelValue: {
+    type: String,
+    required: true,
+  },
+  id: {
+    type: String,
+    required: true,
+    default: "text",
+  },
+  labelText: {
+    type: String,
+    required: true,
+    default: "text",
+  },
+});
+
+const emit = defineEmits(["update:modelValue"]);
+
+function updateModelValue(event) {
+  emit("update:modelValue", event.target.value);
+}
+</script>
+
+<style scoped>
+.password-field-container {
+  display: flex;
+  flex-direction: column;
+}
+
+.password-field-container label {
+  font-size: 14px;
+}
+
+.password-field-container input {
+  height: 32px;
+  border: none;
+  border-bottom: 1px solid #474747;
+  flex: 1;
+}
+
+.password-field-container div {
+  display: flex;
+  align-items: center;
+}
+</style>
